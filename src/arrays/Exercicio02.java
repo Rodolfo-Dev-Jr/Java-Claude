521ec1027b9d21810d31f3aefe918deef0466386
Rodolfo Campos <rodolfo.t.campos@gmail.com>
2026-09-22 17:57:54 -0300

    correção código linha 12 ___for (int i = 0; i < 10; i++) {___  para a correção --> ___for (int i = 0; i < numeros.length; i++) {___

--- src/arrays/Exercicio02.java
+++ src/arrays/Exercicio02.java
@@ -9,7 +9,7 @@ public class Exercicio02 {
             numeros[i] = i * 2;
         }
 
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < numeros.length; i++) {
             System.out.println(numeros[i]);
         }
 
