8e36db75b762f37a1673f70fbb586603d0b54cd1
Rodolfo Campos <rodolfo.t.campos@gmail.com>
2026-09-22 17:53:19 -0300

    feat: arrays - exercicios 01 e 02 (indices, ArrayIndexOutOfBoundsException, preenchimento com for)

---
+++ src/arrays/Exercicio01.java
@@ -0,0 +1,26 @@
+package arrays;
+
+public class Exercicio01 {
+    public static void main(String[] args) {
+
+        int[] notas = new int[5];
+
+        notas[0] = 2;
+        notas[1] = 4;
+        notas[2] = 6;
+        notas[3] = 8;
+        notas[4] = 10;
+//        notas[5] = 12;
+
+        System.out.println(notas[0]);
+        System.out.println(notas[1]);
+        System.out.println(notas[2]);
+        System.out.println(notas[3]);
+        System.out.println(notas[4]);
+//        System.out.println(notas[5]);
+
+        System.out.println(notas.length);
+
+    }
+    
+}
---
+++ src/arrays/Exercicio02.java
@@ -0,0 +1,17 @@
+package arrays;
+
+public class Exercicio02 {
+    public static void main(String[] args) {
+
+        int[] numeros = new int[10];
+
+        for (int i = 0; i < numeros.length; i++) {
+            numeros[i] = i * 2;
+        }
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println(numeros[i]);
+        }
+
+    }
+}
